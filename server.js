dd132c23d7f5493066bc9167264ccd08b2bab15a
Almas Yoko_office <Almas almas@yoko.space>
2026-03-07 00:41:45 +0500

    feat(esling, prettier): Добавил ESLINT и чутка улучшил синтаксис сервера

--- server.js
+++ server.js
@@ -1,42 +1,32 @@
 import express from "express";
+import sqlite3 from "sqlite3";
+import cluster from "node:cluster";
+import { open } from "sqlite";
+import { Server } from "socket.io";
 import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
-import { Server } from "socket.io";
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
 import { availableParallelism } from "node:os";
-import cluster from "node:cluster";
 import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 
-// open the database file
-const db = await open({
-  filename: "chat.db",
-  driver: sqlite3.Database,
-});
+const initDB = async () => {
+  // open the database file
+  const db = await open({
+    filename: "chat.db",
+    driver: sqlite3.Database,
+  });
 
-// create our 'messages' table (you can ignore the 'client_offset' column for now)
-await db.exec(`
-  CREATE TABLE IF NOT EXISTS messages (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      client_offset TEXT UNIQUE,
-      content TEXT
-  );
-`);
+  // create our 'messages' table (you can ignore the 'client_offset' column for now)
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );`);
+  return { db };
+};
 
-if (cluster.isPrimary) {
-  const numCPUs = availableParallelism();
-  // create one worker per available core
-  // I got 16 returned as available cores
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork({
-      PORT: 3000 + i,
-    });
-  }
-
-  // set up the adapter on the primary thread
-  setupPrimary();
-} else {
+const initApp = (db) => {
   const app = express();
   const server = createServer(app);
   const io = new Server(server, {
@@ -50,9 +40,13 @@ if (cluster.isPrimary) {
     res.sendFile(join(__dirname, "index.html"));
   });
 
+  app.get("/detector.js", (req, res) => {
+    res.sendFile(join(__dirname, "detector.js"));
+  });
+
   io.on("connection", async (socket) => {
     socket.on("disconnect", () => {
-      console.log("user disconnected");
+      //   console.log("user disconnected");
     });
 
     socket.on("chat message", async (msg, clientOffset, callback) => {
@@ -88,7 +82,7 @@ if (cluster.isPrimary) {
             socket.emit("chat message", row.content, row.id);
           },
         );
-      } catch (e) {
+      } catch {
         // something went wrong
       }
     }
@@ -97,6 +91,28 @@ if (cluster.isPrimary) {
   const port = process.env.PORT;
 
   server.listen(port, () => {
-    console.log(`server running at http://localhost:${port}`);
+    // console.log(`server running at http://localhost:${port}`);
   });
+};
+
+const initCluster = () => {
+  const numCPUs = availableParallelism();
+  // create one worker per available core
+  // I got 16 returned as available cores
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i,
+    });
+  }
+
+  // set up the adapter on the primary thread
+  setupPrimary();
+};
+
+const { db } = await initDB();
+
+if (cluster.isPrimary) {
+  initCluster();
+} else {
+  initApp(db);
 }
